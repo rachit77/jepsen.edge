1b9089c433f35f142d141e610b9b2f3d817d0661
Rachit Sonthalia <rachitsonthalia02@gmail.com>
2023-05-07 17:30:30 +0530

    changed folder structure and removed a syntax error in cli file

--- src/jepsen/edge/cli.clj
+++ src/jepsen/edge/cli.clj
@@ -9,7 +9,7 @@
 [[nil "--workload WORKLOAD" "Test workload to run; e.g. edge"
     :default :edge
     :parse-fn keyword]
-   (jc/package-opt "edge-url" "https://github.com/0xPolygon/polygon-edge/releases/download/v0.6.3/polygon-edge_0.6.3_linux_amd64.tar.gz")]))
+   (jc/package-opt "edge-url" "https://github.com/0xPolygon/polygon-edge/releases/download/v0.6.3/polygon-edge_0.6.3_linux_amd64.tar.gz")])
                                    
 (defn -main
   "Handles command line arguments. And run a test."
